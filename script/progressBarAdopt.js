5698ad8603d4f3fd464550a773454b5a28605884
Gabriel Rodrigues <gabriel.rodriguesdds@gmail.com>
2024-06-29 12:55:12 -0300

    feat: added comments to make the code clearer

--- script/progressBarAdopt.js
+++ script/progressBarAdopt.js
@@ -3,18 +3,35 @@
 //     window.history.back();
     
 // }
+
+
 function movingArrow1() {
-    const adoptPage1 = document.getElementById("adoptPage1") 
-    adoptPage1.style.display = "none";
+    // ---- do INICIO >>> ENDEREÇO -----
     
+    // ---- Tiarndo Paginas---------
+    const adoptPage1 = document.getElementById("adoptPage1")
+    adoptPage1.style.display = "none";
+
+    const form2 = document.getElementById("form2") 
+    form2.style.display = "none";
+
+    const form3 = document.getElementById("form3") 
+    form3.style.display = "none";
+    
+    const form5 = document.getElementById("form5") 
+    form5.style.display = "none";
+
+
+    
+    // --------------------------------------------------
+
     const form1 = document.getElementById("form1") 
     form1.style.display = "flex";
-
+    
     const orangeArrow = document.getElementById("arrow-start")
     orangeArrow.style.left = "28.6%";
-
+    
     const catIconProgress = document.getElementById("catIconProgress")
-
     catIconProgress.style.visibility = "visible";
     catIconProgress.style.left = "17%";
     catIconProgress.style.bottom = "52%";
@@ -22,21 +39,40 @@ function movingArrow1() {
     const redLine1 = document.getElementById("redLine1")
     redLine1.style.visibility = "visible"
     redLine1.style.color = "#FFB30D"
-
+    
     const redLine2 = document.getElementById("redLine2")
     redLine2.style.visibility = "hidden";
 
+    // ------ Comando para bota a imagem do endereço laranja c/ borda verm
+     
     const localIcon = document.getElementById("localImg")
     localIcon.src = "assets/localconPresent.svg";
     
-    const localIconSelected = document.getElementById("localImgSelected")    
 }
 
+//---------------------    -------------------//
+
+
 function movingArrow2() {
-    // --- Pg2
+    // --- de Endereço >>> Historico de Anim.
+
+    // ---- Tiarndo Paginas---------
+    const adoptPage1 = document.getElementById("adoptPage1") 
+    adoptPage1.style.display = "none";
+    
     const form1 = document.getElementById("form1") 
     form1.style.display = "none";
-    // --- Pg3
+
+    const form3 = document.getElementById("form3") 
+    form3.style.display = "none";
+
+    const form4 = document.getElementById("form4") 
+    form4.style.display = "none";
+
+    const form5 = document.getElementById("form5") 
+    form5.style.display = "none";
+
+// --------------------------------------------------
     const form2 = document.getElementById("form2") 
     form2.style.display = "flex";
 
@@ -55,6 +91,8 @@ function movingArrow2() {
     redLine2.style.visibility = "visible";
     redLine2.style.color = "#FFB30D";
 
+    // ------ Trocando endereço para laranja e Histórico para selecionado
+
     const localIcon = document.getElementById("localImg")
     localIcon.src = "assets/localIconOrange.svg";
     
@@ -64,11 +102,24 @@ function movingArrow2() {
     
 }
 
+
 function movingArrow3() {
- // --- Pg4
+
+ // --- de Historico de Anim. >>>> Motivo da adoção
+// ---- Tiarndo Paginas---------
     const form2 = document.getElementById("form2") 
     form2.style.display = "none";
- // --- Pg5
+    
+    const form1 = document.getElementById("form1") 
+    form1.style.display = "none";
+    
+    const form4 = document.getElementById("form4") 
+    form4.style.display = "none";
+    
+    const form5 = document.getElementById("form5") 
+    form5.style.display = "none";
+    
+    // ----------------------------------
     const form3 = document.getElementById("form3") 
     form3.style.display = "flex";
 
@@ -76,33 +127,39 @@ function movingArrow3() {
     orangeArrow.style.left = "64.5%";
 
     const catIconProgress = document.getElementById("catIconProgress")
-
     catIconProgress.style.left = "53%";
     catIconProgress.style.bottom = "52%";
     
     const redLine4 = document.getElementById("redLine4")
     redLine4.style.visibility = "hidden";
 
+    const redLine3 = document.getElementById("redLine3")
+    redLine3.style.visibility = "visible";
+    redLine3.style.color = "#FFB30D";
+
+    // ------ Trocando historico animais para laranja e motido de adoçao para selecionado
     const pawIcon1 = document.getElementById("pawIcon1")
     pawIcon1.src = "assets/pawOrange.svg";
 
     const pawIcon2 = document.getElementById("pawIcon2")
     pawIcon2.src = "assets/pawIconSelected.svg";
 
-    const redLine3 = document.getElementById("redLine3")
-    redLine3.style.visibility = "visible";
-    redLine3.style.color = "#FFB30D";
 }
 
 function movingArrow4() {
- // --- Pg4
+ // --- de Motivação de adoção >>> Composição familiar
+// ---- Tirando Paginas---------
     const form3 = document.getElementById("form3") 
     form3.style.display = "none";
-// --- Pg5
+    
+    const form5 = document.getElementById("form5") 
+    form5.style.display = "none";
+// -----------------------------------------------
     const form4 = document.getElementById("form4") 
     form4.style.display = "flex";
 
     const orangeArrow = document.getElementById("arrow-start")
+    orangeArrow.style.visibility = "visible";
     orangeArrow.style.left = "82.5%";
 
     const catIconProgress = document.getElementById("catIconProgress")
@@ -113,23 +170,27 @@ function movingArrow4() {
     const redLine5 = document.getElementById("redLine5")
     redLine5.style.visibility = "hidden";
 
+    const redLine4 = document.getElementById("redLine4")
+    redLine4.style.visibility = "visible";
+    redLine4.style.color = "#FFB30D";
+
+    // ------ Trocando  motido de adoçao para laranja e Composição familiar p/ selecionado
+
     const pawIcon2 = document.getElementById("pawIcon2")
     pawIcon2.src = "assets/pawOrange.svg";
     
     const familyIcon = document.getElementById("familyIcon")
     familyIcon.src = "assets/familySelected.svg";
     
-    const redLine4 = document.getElementById("redLine4")
-    redLine4.style.visibility = "visible";
-    redLine4.style.color = "#FFB30D";
 }
 
 function movingArrow5() {
+    // --- de Composição familiar >>>> Confirmação
 
-    // --- Pg4
-    const form4 = document.getElementById("form4") 
+// ---- Tirando Paginas---------
+const form4 = document.getElementById("form4") 
     form4.style.display = "none";
-// --- Pg5
+// --------------------------------------------------------------
     const form5 = document.getElementById("form5") 
     form5.style.display = "flex";
 
@@ -144,26 +205,28 @@ function movingArrow5() {
     redLine5.style.visibility = "visible";
     redLine5.style.color = "#FFB30D";
 
+// ------ Trocando Composição familiar para laranja e Confirmação p/ selecionado
 
-    const checkIcon = document.getElementById("checkIcon")
-    checkIcon.src = "assets/checkSelected.svg";
-    
     const familyIcon = document.getElementById("familyIcon")
     familyIcon.src = "assets/familyOrange.svg";
-
+    
+    const checkIcon = document.getElementById("checkIcon")
+    checkIcon.src = "assets/checkSelected.svg";
 }
 
-function movingArrow6() {
-    const progressBar = document.getElementById("progressContainer")
-    progressBar.style.display = "none";
+// de confirmação para a proxima pagina, so tirar a barra de progresso e deixar vazio
+// ainda não terminei o codigo
+// function movingArrow6() {
+//     const progressBar = document.getElementById("progressContainer")
+//     progressBar.style.display = "none";
 
-    const form5 = document.getElementById("form5") 
-    form5.style.display = "none";
-// --- Pg5
-    const form6 = document.getElementById("form6") 
-    form6.style.display = "flex";
+//     const form5 = document.getElementById("form5") 
+//     form5.style.display = "none";
+
+//     const form6 = document.getElementById("form6") 
+//     form6.style.display = "flex";
     
 
 
-}
+// }
 
